66def8b6b1067891ae94f169571ae838fce5ea14
DT-1236 <WEMascarinas@gmail.com>
2019-01-23 16:11:53 -0800

    default props, bootstrap classes, and redirect for missing post

--- src/Components/PostView.js
+++ src/Components/PostView.js
@@ -3,43 +3,54 @@ import { Link } from 'react-router-dom';
 import CommentsContainer from '../Containers/CommentsContainer';
 
 class PostView extends Component {
+  componentDidMount() {
+    const { description, history } = this.props;
+    if (!description) {
+      history.replace('/');
+    }
+  }
+
   render() {
     const { title, description, body, id, remove, history } = this.props;
     return (
-      <div>
-        <div>
+      <div className="d-flex justify-content-center">
+        <div className="card bg-light mb-3">
           <div>
-            <h1>{title}</h1>
-            <h5>{description}</h5>
+            <div className="card-header">
+              <h1>{title}</h1>
+              <h5>{description}</h5>
+            </div>
+            <div className="card-body">
+              <Link name="edit" to={`/${id}/edit`}>
+                <i className="fas fa-edit" />
+              </Link>
+              <button
+                className="border-0 text-danger"
+                name="delete"
+                onClick={() => {
+                  remove({ id });
+                  history.replace('/');
+                }}
+              >
+                <i className="fas fa-trash-alt" />
+              </button>
+            </div>
           </div>
-          <div>
-            <Link name="edit" to={`/${id}/edit`}>
-              <i className="fas fa-edit" />
-            </Link>
-            <button
-              name="delete"
-              onClick={() => {
-                remove({ id });
-                history.replace('/');
-              }}
-            >
-              <i className="fas fa-trash-alt" />
-            </button>
-          </div>
+          <p>{body}</p>
+          <hr />
+          <CommentsContainer postId={id} />
         </div>
-        <p>{body}</p>
-        <hr />
-        <CommentsContainer postId={id} />
       </div>
     );
   }
 }
 
 PostView.defaultProps = {
-  id: 1,
-  title: 'TEST TITLE',
-  description: 'Some kind of test',
-  body: 'Eeven more teeeeext'
+  comments: {}
+  //   id: 1,
+  //   title: 'TEST TITLE',
+  //   description: 'Some kind of test',
+  //   body: 'Eeven more teeeeext'
 };
 
 export default PostView;
