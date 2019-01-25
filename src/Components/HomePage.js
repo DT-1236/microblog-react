cf42e3904ede63e4aaebcf7e8d6043f7903accd8
DT-1236 <WEMascarinas@gmail.com>
2019-01-24 17:29:27 -0800

    NavBarContainer removed. Expanded HomePageContainer

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import PostCard from './PostCard';
 
 class HomePage extends Component {
+  componentDidMount() {
+    this.props.getPostsAPI();
+  }
+
   renderPosts() {
     const { titles } = this.props;
     return Object.keys(titles).map(id => (
@@ -9,6 +13,9 @@ class HomePage extends Component {
     ));
   }
   render() {
+    if (this.props.loading) {
+      return <p>Loading...</p>;
+    }
     return (
       <div>
         <div className="container">
