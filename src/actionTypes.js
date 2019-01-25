efa1fc789fff4ff25b0f1f04cdfdc3f2ca876f2a
DT-1236 <WEMascarinas@gmail.com>
2019-01-24 17:14:13 -0800

    Added GET_POST

--- src/actionTypes.js
+++ src/actionTypes.js
@@ -4,3 +4,4 @@ export const EDIT = 'EDIT';
 export const ADD_COMMENT = 'ADD_COMMENT';
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 export const LOAD_POSTS = 'LOAD_POSTS';
+export const GET_POST = 'GET_POST';
